c8390b5f9265dfebdeb601b0471c62cbd7a5d111
Mathilde Robert <m.robert@skema.edu>
2019-11-26 14:39:06 +0100

    show infos

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,5 +3,12 @@ class MoviesController < ApplicationController
   end
 
   def show
+    @movie = Movie.find(params[:id])
+  end
+
+  private
+
+  def movies_params
+    params.require(:movie).permit(:title, :description, :category, :director, :cast, :duration, :rating)
   end
 end
