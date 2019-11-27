013b01e0dac8042625d89efe7d907386401b78f1
Rija Rajohnson <rija.rajohnson@gmail.com>
2019-11-27 11:08:37 +0100

    fix show baby

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,12 +2,11 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
     @theaters = Theater.geocoded.near([params[:lng], params[:lat]], 50)
-    raise
   end
 
   def show
     @movie = Movie.find(params[:id])
-    @theater = Theater.find(params[:id]) #à modifier quand on aura la géoloc : chercher les cinémas dont la distance avec le current_user est < à 5km + afficher les cinémas par ordre croissant de distance dans la show).
+    @theater = Theater.geocoded #à modifier quand on aura la géoloc : chercher les cinémas dont la distance avec le current_user est < à 5km + afficher les cinémas par ordre croissant de distance dans la show).
     @showtimes = Showtime.where(movie_id: @movie, theater_id: @theater)
   end
 
