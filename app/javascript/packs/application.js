f82a956806875643e39ee71f577e43452a88f3c4
Mathilde Robert <m.robert@skema.edu>
2019-11-27 18:39:53 +0100

    js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,26 +8,6 @@ import { geoloc } from "../plugins/geoloc";
 initAutocomplete();
 initSiema();
 
-if (document.querySelector('.geoloc')) {
-  const button = document.querySelector('.geoloc');
-  button.addEventListener('click', (event) => {
-    event.preventDefault();
-    navigator.geolocation.getCurrentPosition(successCallback,errorCallback,{timeout:10000});
-  });
-
-  const successCallback = (event) => {
-    const crd = event.coords;
-    const lat = crd.latitude;
-    const long = crd.longitude;
-    window.location.replace(`http://localhost:3000/movies?lng=${long}&lat=${lat}`);
-  };
-
-  const errorCallback = (err) => {
-    console.warn(`ERREUR (${err.code}): ${err.message}`);
-  };
-}
-
-
 
 if (document.querySelector('.geoloc')) {
   geoloc();
