43ebd75089d82a3d97215163dd74c8bab729511e
Mathilde Robert <m.robert@skema.edu>
2019-11-28 19:58:15 +0100

    confirmation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
   def body_class_names
     return "body-bg" if action_name == "home"
+    return "conf-bg" if controller_name == "orders" && action_name == "show"
   end
 
   def loc
