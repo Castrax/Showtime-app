6df13e108a91391dd2d1d7f5982e211659c3987c
Jonathan Santurbano <jonathan.santurbano@gmail.com>
2019-11-28 15:26:38 +0100

    automatic total price with +/-

---
+++ app/javascript/plugins/calculate_price.js
@@ -0,0 +1,33 @@
+const calculatePrice = () => {
+
+  console.log("salut");
+
+  let number_of_seats_string = document.querySelector('#number-of-seats');
+  let counter = 1
+  const plus = document.querySelector('#plus');
+  const minus = document.querySelector('#minus');
+  const price_per_seat_string = document.querySelector(".price-per-seat");
+  const total_price = document.querySelector(".total-price");
+
+
+  plus.addEventListener("click", (event) => {
+    counter += 1;
+    number_of_seats_string.value = counter;
+    total_price.innerText = parseInt(price_per_seat_string.innerText) * parseInt(number_of_seats_string.value);
+  });
+  minus.addEventListener("click", (event) => {
+    counter -= 1;
+    number_of_seats_string.value = counter;
+    total_price.innerText = parseInt(price_per_seat_string.innerText) * parseInt(number_of_seats_string.value);
+  });
+  number_of_seats_string.addEventListener('input', (event) => {
+    console.log(total_price.innerText);
+    if(number_of_seats_string.value == ""){
+      total_price.innerText = 0
+    } else {
+      total_price.innerText = parseInt(price_per_seat_string.innerText) * parseInt(number_of_seats_string.value);
+    }
+  });
+};
+
+export { calculatePrice };
