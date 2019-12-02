fa6d46f5b5aa8cd3667003baf19e4a91ce1a0f01
Jonathan Santurbano <jonathan.santurbano@gmail.com>
2019-12-02 12:44:22 +0100

    order amount updates with modal total price from show

--- app/javascript/plugins/calculate_price.js
+++ app/javascript/plugins/calculate_price.js
@@ -6,26 +6,27 @@ const calculatePrice = () => {
   const minus = document.querySelector('#minus');
   const price_per_seat_string = document.querySelector(".price-per-seat");
   const total_price = document.querySelector(".total-price");
+  const stripeSeats = document.querySelector("#number_of_seats");
 
 
   plus.addEventListener("click", (event) => {
-    console.log(event);
     counter += 1;
     number_of_seats_string.value = counter;
     total_price.innerText = parseInt(price_per_seat_string.innerText) * parseInt(number_of_seats_string.value);
+    stripeSeats.value = number_of_seats_string.value;
   });
   minus.addEventListener("click", (event) => {
-    console.log(event);
     counter -= 1;
     number_of_seats_string.value = counter;
     total_price.innerText = parseInt(price_per_seat_string.innerText) * parseInt(number_of_seats_string.value);
+    stripeSeats.value = number_of_seats_string.value;
   });
   number_of_seats_string.addEventListener('input', (event) => {
-    console.log(total_price.innerText);
     if(number_of_seats_string.value == ""){
       total_price.innerText = 0
     } else {
       total_price.innerText = parseInt(price_per_seat_string.innerText) * parseInt(number_of_seats_string.value);
+      stripeSeats.value = number_of_seats_string.value;
     }
   });
 };
