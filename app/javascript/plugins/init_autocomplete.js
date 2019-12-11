7d907ce90c19f2489a993d55920130223f5b6b6e
Mathilde Robert <m.robert@skema.edu>
2019-12-11 22:42:02 +0100

    geoloc hp

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -3,8 +3,22 @@ import places from 'places.js';
 const initAutocomplete = () => {
   const addressInput = document.getElementById('autocomplete_address');
   if (addressInput) {
-    places({ container: addressInput });
-  }
+    const placesAutocomplete = places({ container: addressInput });
+    const button = document.querySelector('.btn-home');
+    placesAutocomplete.on('change', (event) => {
+      const lat = event.suggestion.latlng.lat;
+      const lng = event.suggestion.latlng.lng;
+      button.disabled = false;
+      button.addEventListener('click', (event) => {
+        event.preventDefault();
+        window.location.assign(`/movies?lng=${lng}&lat=${lat}`);
+      });
+    });
+    if (addressInput.value == "") {
+      button.disabled = true;
+    };
+  };
 };
+
 
 export { initAutocomplete };
