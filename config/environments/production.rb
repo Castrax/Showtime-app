b02d2d84947db20126e3d8d92ec75a4c4e7a8d99
Mathilde Robert <m.robert@skema.edu>
2020-11-30 12:34:27 +0100

    new domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "https://www.showtime-app.co" }
+  config.action_mailer.default_url_options = { host: "https://www.showtime-app.xyz" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
